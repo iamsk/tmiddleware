83e261ae4793a22757de43278008396a8f1f1e05
iamsk <iamsk.info@gmail.com>
2013-07-02 12:22:04 +0800

    support for redis

--- examples/backend.py
+++ examples/backend.py
@@ -1,8 +1,10 @@
-from memcache import Client
-
-
 class Memcache(object):
+    """
+    address=127.0.0.1:11211
+    """
     def __init__(self, address):
+        from memcache import Client
+
         self.conn = Client([address])
 
     def get(self, key):
@@ -10,6 +12,26 @@ class Memcache(object):
 
     def set(self, key, value, timeout):
         self.conn.set(key, value, timeout)
+
+    def delete(self, key):
+        self.conn.delete(key)
+
+
+class Redis(object):
+    """
+    address=['1270.0.1', 6379, 0]
+    """
+    def __init__(self, address):
+        from redis import Redis
+
+        self.conn = Redis(host=address[0], port=address[1], db=address[2])
+
+    def get(self, key):
+        return self.conn.get(key)
+
+    def set(self, key, value, timeout):
+        self.conn.set(key, value)
+        self.conn.expire(key, timeout)
 
     def delete(self, key):
         self.conn.delete(key)
